e11c0f6c5cea3a2b7366df6e0160e31ddd9a090d
Dhanshri911 <dszingade911@gmail.com>
2022-03-24 20:40:13 +0530

    Emp is Present or Not

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,6 +1,11 @@
 public class EmployeeWage {
     public static void main(String[] arr){
-        System.out.println("Welcome..");
+       int empIsPresent = (int) (Math.floor(Math.random() * 10 ) %2);
+       if(empIsPresent == 1){
+           System.out.println("Employee is Present");
+       }else {
+           System.out.println("Employee is Absent");
+       }
 
     }
 }

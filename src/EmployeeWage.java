3ea5f8702f4ef0955950a9ff20fd31f2b84100ce
Dhanshri911 <dszingade911@gmail.com>
2022-03-24 20:51:01 +0530

    UC2:Added Employee Daily Wages

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,11 +1,42 @@
 public class EmployeeWage {
-    public static void main(String[] arr){
-       int empIsPresent = (int) (Math.floor(Math.random() * 10 ) %2);
-       if(empIsPresent == 1){
-           System.out.println("Employee is Present");
-       }else {
-           System.out.println("Employee is Absent");
-       }
+    static final int WAGE_PER_HOUR = 20;
+    static final int FULL_DAY_HOUR = 8;
 
+    public static void main(String[] args) {
+
+        int empIsPresent = (int) (Math.floor(Math.random() * 10) % 2);
+        int empWage = 0;
+        if (empIsPresent == 1) {
+            System.out.println("Employee is Present");
+            empWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+        } else {
+            System.out.println("Employee is Absent");
+        }
+        System.out.println("Employee Wage:" + empWage);
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
